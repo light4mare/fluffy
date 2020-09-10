52b14a30050b01f13c1aaf042ac5570a131a4cd7
stormythought@163.com <13126065403@wx>
2020-09-10 23:08:33 +0800

    selector

--- widget/selector_builder.dart
+++ widget/selector_builder.dart
@@ -1,22 +1,59 @@
-
-
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-typedef SelectorWidgetBuilder = Widget Function(BuildContext context, StateSetter setState);
+// https://blog.csdn.net/u013894711/article/details/102785532
 
-class SelectorBuilder extends StatefulWidget {
-  const SelectorBuilder(this._builder);
+class SelectorBuilder<DATA, VM> extends StatefulWidget {
+  const SelectorBuilder({this.builder, this.selector});
 
-  final SelectorWidgetBuilder _builder;
+  // 这个地方返回具体的值，对应builder中的data
+  final DATA Function(BuildContext, VM) selector;
+  final ValueWidgetBuilder<DATA> builder;
 
   @override
   State<StatefulWidget> createState() => _SelectorState();
 }
 
-class _SelectorState extends State<SelectorBuilder> {
+class _SelectorState<DATA, VM> extends State<SelectorBuilder<DATA, VM>> {
   @override
   Widget build(BuildContext context) {
-    return widget._builder.call(context, null);
+    return ChangeNotifierProvider.value(
+      value: null,
+      child: Selector<VM, DATA>(
+        selector: widget.selector,
+        builder: widget.builder,
+      ),
+    );
+    ;
+  }
+}
+
+class Message {
+  var name = '';
+  var age = 0;
+}
+
+class ViewModel extends ChangeNotifier {
+  void onCallback(){
+
   }
 
+  String getName(){
+    return "";
+  }
+
+  Message getMessage(){
+    return Message();
+  }
+}
+
+void testSelector() {
+  SelectorBuilder<Message, ViewModel>(
+    builder: (context, data, child) {
+      return Text(data.name);
+    },
+    selector: (context, viewModel) {
+      return viewModel.getMessage();
+    },
+  );
 }
